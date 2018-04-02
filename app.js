74fbc57eda6374f8aac2a0f04ca4dcb8960935be
Alfadiktator <alfadiktator@gmail.com>
2018-04-02 20:14:49 +0300

    update server

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => { res.sendFile(path.resolve('public/index.html')); });
-
-app.get('/public/*', (req, res) => {
-  //console.log(req);
-  res.sendFile(path.resolve('./' + req.url))
-});
+app.use(express.static('public'));
 
 const server = app.listen(8000, () => {
   console.log(`Server started working`);
